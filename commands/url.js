832b04c3d2451ee5909177908d51921641f77a99
spacetastic <72137105+space-tastic@users.noreply.github.com>
2021-11-19 09:39:48 +0000

    fixed some bugs

--- commands/url.js
+++ commands/url.js
@@ -1,10 +1,12 @@
 module.exports = (client) => async(event) =>{
   try{
-    await client.editMessage(event.message.chatId, {
-      message : event.message.id,
-      text:event.message.message,
-      linkPreview:false,
-      parseMode:"html"
-    });
+    if(event.message.message[0] != "*"){
+      await client.editMessage(event.message.chatId, {
+        message : event.message.id,
+        text:event.message.message,
+        linkPreview:false,
+        parseMode:"html"
+      });
+    }
   }catch(err){}
 }
--- index.js
+++ index.js
@@ -28,15 +28,14 @@ const meaning = require("./commands/meaning");
   await client.connect();
   console.log("--------------BOT IS WORKING--------------")
 
-  client.addEventHandler(alive(client), new NewMessage({ outgoing:true, pattern:/^.alive/ }));
-  client.addEventHandler(spam(client), new NewMessage({ outgoing:true, pattern:/^.spam */}));
-  client.addEventHandler(weather(client), new NewMessage({ outgoing:true, pattern:/^.weather */ }));
-  client.addEventHandler(yt(client), new NewMessage({ outgoing:true, pattern:/^.yt */ }));
-  client.addEventHandler(speak(client), new NewMessage({ outgoing:true, pattern:/^.speak */ }));
-  client.addEventHandler(pic(client), new NewMessage({ outgoing:true, pattern:/^.pic */ }));
-  client.addEventHandler(sticker(client), new NewMessage({ outgoing:true, pattern:/^.sticker */ }));
-  // client.addEventHandler(url(client), new NewMessage({outgoing:true, pattern:/^.url */}));
-  client.addEventHandler(meaning(client), new NewMessage({outgoing:true, pattern:/^.meaning */}));
+  client.addEventHandler(alive(client), new NewMessage({ outgoing:true, pattern:/\*alive/ }));
+  client.addEventHandler(spam(client), new NewMessage({ outgoing:true, pattern:/\*spam */}));
+  client.addEventHandler(weather(client), new NewMessage({ outgoing:true, pattern:/\*weather */ }));
+  client.addEventHandler(yt(client), new NewMessage({ outgoing:true, pattern:/\*yt */ }));
+  client.addEventHandler(speak(client), new NewMessage({ outgoing:true, pattern:/\*speak */ }));
+  client.addEventHandler(pic(client), new NewMessage({ outgoing:true, pattern:/\*pic */ }));
+  client.addEventHandler(sticker(client), new NewMessage({ outgoing:true, pattern:/\*sticker */ }));
+  client.addEventHandler(meaning(client), new NewMessage({outgoing:true, pattern:/\*meaning */}));
   client.addEventHandler(url(client), new NewMessage({outgoing:true}));
   
 })()
